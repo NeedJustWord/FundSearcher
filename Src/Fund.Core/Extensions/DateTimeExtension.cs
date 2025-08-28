abff35d3838d410fc424945667605d023145c6d5
NeedJustWord <13718169+NeedJustWord@users.noreply.github.com>
2025-08-28 23:17:15 +0800

    格式化成立日期的显示，添加X年X个月的描述

---
+++ Src/Fund.Core/Extensions/DateTimeExtension.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Fund.Core.Extensions
+{
+    public static class DateTimeExtension
+    {
+        /// <summary>
+        /// 获取指定时间相差多少年多少个月
+        /// </summary>
+        /// <param name="date"></param>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public static string GetCreateYearAndMonth(this DateTime date, DateTime other)
+        {
+            DateTime min, max;
+            if (date > other)
+            {
+                max = date;
+                min = other;
+            }
+            else
+            {
+                max = other;
+                min = date;
+            }
+
+            int year = max.Year, month = max.Month;
+            if (max.Month < min.Month)
+            {
+                year--;
+                month += 12;
+            }
+
+            year -= min.Year;
+            month -= min.Month;
+
+            return year == 0 ? $"{month}个月" : (month == 0 ? $"{year}年" : $"{year}年{month}个月");
+        }
+    }
+}
---
+++ Src/FundSearcher/Converters/BirthDayConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using Fund.Core.Extensions;
+
+namespace FundSearcher.Converters
+{
+    class BirthDayConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var date = (DateTime?)value;
+            return date.HasValue ? $"{date.Value:yyyy-MM-dd}({date.Value.GetCreateYearAndMonth(DateTime.Now)})" : "---";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
