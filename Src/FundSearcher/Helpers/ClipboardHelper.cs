87674ca16e53388b3dbd3827d9bf4953ff662271
NeedJustWord <13718169+NeedJustWord@users.noreply.github.com>
2026-01-28 10:58:39 +0800

    复制到剪切板失败时才释放申请的非托管内存，成功时不释放，解决错误释放导致程序崩溃的问题

--- Src/FundSearcher/Helpers/ClipboardHelper.cs
+++ Src/FundSearcher/Helpers/ClipboardHelper.cs
@@ -5,7 +5,6 @@ namespace FundSearcher.Helpers
 {
     /// <summary>
     /// 剪贴板帮助类
-    /// <para>https://learn.microsoft.com/zh-cn/windows/win32/api/winuser/nf-winuser-openclipboard</para>
     /// </summary>
     static class ClipboardHelper
     {
@@ -20,15 +19,24 @@ namespace FundSearcher.Helpers
             {
                 return false;
             }
-            EmptyClipboard();
 
-            IntPtr ptr = IntPtr.Zero;
             try
             {
-                ptr = Marshal.StringToHGlobalUni(text);
-                SetClipboardData(Format.CF_UNICODETEXT, ptr);
-                CloseClipboard();
-                return true;
+                if (!EmptyClipboard())
+                {
+                    return false;
+                }
+
+                var ptr = Marshal.StringToHGlobalUni(text);
+                if (SetClipboardData(CF_UNICODETEXT, ptr) == IntPtr.Zero)
+                {
+                    Marshal.FreeHGlobal(ptr);
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
             }
             catch (Exception)
             {
@@ -36,15 +44,15 @@ namespace FundSearcher.Helpers
             }
             finally
             {
-                if (ptr != IntPtr.Zero) Marshal.FreeHGlobal(ptr);
+                CloseClipboard();
             }
         }
 
-        #region Windows API
+        #region 剪贴板API https://learn.microsoft.com/zh-cn/windows/win32/api/winuser/nf-winuser-openclipboard
         /// <summary>
         /// 打开剪贴板
         /// </summary>
-        /// <param name="hWndNewOwner"></param>
+        /// <param name="hWndNewOwner">要与打开的剪贴板关联的窗口的句柄。 如果此参数为 NULL，则打开的剪贴板与当前任务相关联</param>
         /// <returns></returns>
         [DllImport("User32")]
         public static extern bool OpenClipboard(IntPtr hWndNewOwner);
@@ -66,7 +74,7 @@ namespace FundSearcher.Helpers
         /// <summary>
         /// 确定剪贴板是否包含指定格式的数据
         /// </summary>
-        /// <param name="format"></param>
+        /// <param name="format">标准或已注册的剪贴板格式</param>
         /// <returns></returns>
         [DllImport("User32")]
         public static extern bool IsClipboardFormatAvailable(uint format);
@@ -74,31 +82,32 @@ namespace FundSearcher.Helpers
         /// <summary>
         /// 从剪贴板中检索指定格式的数据
         /// </summary>
-        /// <param name="format"></param>
-        /// <returns></returns>
+        /// <param name="format">剪贴板格式</param>
+        /// <returns>
+        /// 如果函数成功，则返回值是指定格式的剪贴板对象的句柄。
+        /// 如果函数失败，则返回值为 NULL。 要获得更多的错误信息，请调用 GetLastError。
+        /// </returns>
         [DllImport("User32")]
         public static extern IntPtr GetClipboardData(uint format);
 
         /// <summary>
         /// 将数据以指定的剪贴板格式放置在剪贴板上
         /// </summary>
-        /// <param name="format"></param>
-        /// <param name="hMem"></param>
-        /// <returns></returns>
+        /// <param name="format">剪贴板格式</param>
+        /// <param name="hMem">指定格式的数据的句柄</param>
+        /// <returns>
+        /// 如果函数成功，则返回值是数据的句柄。
+        /// 如果函数失败，则返回值为 NULL。 要获得更多的错误信息，请调用 GetLastError。
+        /// </returns>
         [DllImport("User32", CharSet = CharSet.Unicode)]
         public static extern IntPtr SetClipboardData(uint format, IntPtr hMem);
+        #endregion
 
+        #region 剪贴板格式 https://learn.microsoft.com/zh-cn/windows/win32/dataxchg/standard-clipboard-formats
         /// <summary>
-        /// 剪贴板格式
-        /// <para>https://learn.microsoft.com/zh-cn/windows/win32/dataxchg/standard-clipboard-formats</para>
+        /// Unicode文本格式
         /// </summary>
-        class Format
-        {
-            /// <summary>
-            /// Unicode 文本格式
-            /// </summary>
-            public const uint CF_UNICODETEXT = 13;
-        }
+        private const uint CF_UNICODETEXT = 13;
         #endregion
     }
 }
