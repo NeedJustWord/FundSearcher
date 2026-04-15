faaeaf1075ae25b13733561994dd68279235a692
NeedJustWord <13718169+NeedJustWord@users.noreply.github.com>
2026-04-15 17:15:05 +0800

    搜索框添加水印

---
+++ Src/FundSearcher/Attacheds/TextBoxAttached.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace FundSearcher.Attacheds
+{
+    internal class TextBoxAttached
+    {
+        #region 水印
+        public static string GetWaterMark(DependencyObject obj)
+        {
+            return (string)obj.GetValue(WaterMarkProperty);
+        }
+
+        public static void SetWaterMark(DependencyObject obj, string value)
+        {
+            obj.SetValue(WaterMarkProperty, value);
+        }
+
+        public static readonly DependencyProperty WaterMarkProperty =
+            DependencyProperty.RegisterAttached("WaterMark", typeof(string), typeof(TextBoxAttached), new PropertyMetadata(""));
+        #endregion
+    }
+}
